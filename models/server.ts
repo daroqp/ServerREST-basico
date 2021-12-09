45f057d5a8390e46687f6a9a9f85b0cd48731ba7
Dario Quispe <daro.qp@gmail.com>
2021-12-09 01:00:03 -0300

    Server REST basicModel

---
+++ models/server.ts
@@ -0,0 +1,38 @@
+import express, { Application } from 'express';
+import userRoutes from '../routes/usuario';
+import cors from 'cors';
+
+export class Server {
+
+    private app: Application;
+    private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || '8000';
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+    
+        this.app.use( cors() );
+        this.app.use( express.json() );
+        this.app.use( express.static('public') );
+    
+    }
+
+    routes() {
+        this.app.use( this.apiPaths.usuarios, userRoutes);
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log("Server running on port: " + this.port);
+        })
+    }
+}
